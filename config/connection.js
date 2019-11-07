ec95865c12b8bd038eb195b2c153f3cbf2393788
KuTu <haamraah@yahoo.com>
2019-11-07 01:32:09 -0700

    database JAWS info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "kazn3ftkb59i1v87",
-  password: "pmm40igkm82ew53a",
-  database: "g5r4ycx0kb5fj4ry"
-});
+var connection;
+
+if( process.env.JAWSDB_URL){
+  connection= mysql.createConnection( process.env.JAWSDB_URL);
+}else {
+connection  = mysql.createConnection({
+   host: "localhost",
+   port: 3306,
+   user: "root",
+   password: "zap",
+   database: "burgers_db"
+ });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
